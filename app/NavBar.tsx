f94a695cd7926ea0c5ef7984244ee319926c1fe1
Tiago Guilherme <tiagodevesse2002@gmail.com>
2026-06-17 22:58:36 +0200

    Build the new issue page

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,11 +1,9 @@
 'use client'; 
 import React from 'react';
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
 import { AiFillBug } from 'react-icons/ai';
 
 const NavBar = () => {
-    const currentPath = usePathname();
   return (
     <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
         <Link href="/" className="flex items-center space-x-2">
